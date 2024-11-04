7b96b39b4a82b93adb03181a29726c6c1cedf2d0
anulabgit <127391777+anulabgit@users.noreply.github.com>
2024-11-04 11:31:14 +0900

    Add files via upload

---
+++ MainWindow.xaml.cpp
@@ -0,0 +1,49 @@
+#include "pch.h"
+#include "MainWindow.xaml.h"
+#if __has_include("MainWindow.g.cpp")
+#include "MainWindow.g.cpp"
+#endif
+
+using namespace winrt;
+using namespace Microsoft::UI::Xaml;
+
+// To learn more about WinUI, the WinUI project structure,
+// and more about our project templates, see: http://aka.ms/winui-project-info.
+
+namespace winrt::App4::implementation
+{
+    MainWindow::MainWindow()
+    {
+        InitializeComponent();
+    }
+
+    int32_t MainWindow::MyProperty()
+    {
+        throw hresult_not_implemented();
+    }
+
+    void MainWindow::MyProperty(int32_t /* value */)
+    {
+        throw hresult_not_implemented();
+    }
+}
+float px(100), py(100); // for mouse point
+using namespace Microsoft::Graphics::Canvas::UI::Xaml; // CanvasControl
+struct winrt::Windows::UI::Color mYcol = winrt::Microsoft::UI::Colors::Green();
+void winrt::App4::implementation::MainWindow::CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args)
+{
+    CanvasControl canvas = sender.as<CanvasControl>();
+    args.DrawingSession().DrawEllipse(px, py, 80, 30, mYcol, 8);
+    args.DrawingSession().DrawText(L"Hello, world!", px - 55, py - 15, winrt::Microsoft::UI::Colors::Yellow());
+}
+void winrt::App4::implementation::MainWindow::CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e)
+{
+    CanvasControl canvas = sender.as<CanvasControl>();
+    px = e.GetCurrentPoint(canvas).Position().X;
+    py = e.GetCurrentPoint(canvas).Position().Y;
+    canvas.Invalidate(); // Call draw cavas
+}
+void winrt::App4::implementation::MainWindow::ColorPicker_ColorChanged(winrt::Microsoft::UI::Xaml::Controls::ColorPicker const& sender, winrt::Microsoft::UI::Xaml::Controls::ColorChangedEventArgs const& args)
+{
+    mYcol = args.NewColor();
+}
---
+++ MainWindow.xaml.h
@@ -0,0 +1,31 @@
+// Copyright (c) Microsoft Corporation and Contributors.
+// Licensed under the MIT License.
+
+#pragma once
+
+#include "MainWindow.g.h"
+#include <winrt/Microsoft.Graphics.Canvas.UI.Xaml.h>
+#include <winrt/Microsoft.UI.Xaml.Input.h>
+#include <winrt/Microsoft.UI.Input.h>
+
+namespace winrt::App4::implementation
+{
+    struct MainWindow : MainWindowT<MainWindow>
+    {
+        MainWindow();
+        int32_t MyProperty();
+        void MyProperty(int32_t value);
+
+        // void myButton_Click(Windows::Foundation::IInspectable const& sender, Microsoft::UI::Xaml::RoutedEventArgs const& args);
+        void CanvasControl_Draw(winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasControl const& sender, winrt::Microsoft::Graphics::Canvas::UI::Xaml::CanvasDrawEventArgs const& args);
+        void CanvasControl_PointerMoved(winrt::Windows::Foundation::IInspectable const& sender, winrt::Microsoft::UI::Xaml::Input::PointerRoutedEventArgs const& e);
+        void ColorPicker_ColorChanged(winrt::Microsoft::UI::Xaml::Controls::ColorPicker const& sender, winrt::Microsoft::UI::Xaml::Controls::ColorChangedEventArgs const& args);
+    };
+}
+
+namespace winrt::App4::factory_implementation
+{
+    struct MainWindow : MainWindowT<MainWindow, implementation::MainWindow>
+    {
+    };
+}
